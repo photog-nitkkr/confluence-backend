abff53755a03aaf65722644ca3cd02150dfd77a8
Mudit Jain <ciphereck@gmail.com>
2019-08-31 16:47:37 +0530

    Firebase Initialized

--- app.go
+++ app.go
@@ -1,16 +1,39 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
 
+	firestore "cloud.google.com/go/firestore"
 	"github.com/gorilla/mux"
 )
+
 func main() {
+	client := firestoreInitialization()
+	fmt.Println(client)
+
+	http.Handle("/", muxRouterInitialization())
+	http.ListenAndServe(":8080", nil)
+}
+
+func muxRouterInitialization() *mux.Router {
 	muxRouter := mux.NewRouter()
 	muxRouter.HandleFunc("/", MuxHandler)
-	http.Handle("/", muxRouter)
-	http.ListenAndServe(":8080", nil)
+	return muxRouter
+}
+
+func firestoreInitialization() *firestore.Client {
+	ctx := context.Background()
+	projectId := "confluence-backend"
+	firebaseError := "Failed to create firebase client with err:%v"
+
+	client, err := firestore.NewClient(ctx, projectId)
+	if err != nil {
+		log.Fatalln(firebaseError, err)
+	}
+	return client
 }
 
 func MuxHandler(w http.ResponseWriter, r *http.Request) {
