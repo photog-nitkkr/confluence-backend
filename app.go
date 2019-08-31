e249fe85dd53a7536c6a117ac5c9a27716f22a36
Mudit Jain <ciphereck@gmail.com>
2019-08-31 21:30:44 +0530

    Adding Routing

--- app.go
+++ app.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 
+	"./events"
 	firestore "cloud.google.com/go/firestore"
 	"github.com/gorilla/mux"
 )
@@ -14,20 +15,30 @@ func main() {
 	client := firestoreInitialization()
 	fmt.Println(client)
 
-	http.Handle("/", muxRouterInitialization())
-	http.ListenAndServe(":8080", nil)
+	startListening()
 }
 
-func muxRouterInitialization() *mux.Router {
+func muxRouterInitializer() *mux.Router {
 	muxRouter := mux.NewRouter()
+	muxRouter.PathPrefix("/activities").Handler(http.StripPrefix("/activities", events.Handler()))
 	muxRouter.HandleFunc("/", MuxHandler)
 	return muxRouter
+}
+
+func startListening() {
+	muxRouter := muxRouterInitializer()
+
+	err := http.ListenAndServe(":8080", muxRouter)
+	if err != nil {
+		listeningError := "Error in Listening: "
+		log.Fatalln(listeningError, err)
+	}
 }
 
 func firestoreInitialization() *firestore.Client {
 	ctx := context.Background()
 	projectId := "confluence-backend"
-	firebaseError := "Failed to create firebase client with err:%v"
+	firebaseError := "Failed to create firebase client with err: "
 
 	client, err := firestore.NewClient(ctx, projectId)
 	if err != nil {
@@ -38,5 +49,9 @@ func firestoreInitialization() *firestore.Client {
 
 func MuxHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "It's working with mux")
+	_, err := fmt.Fprintf(w, "It's working with mux")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
