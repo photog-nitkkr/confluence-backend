7a7a9e99eb873ef3b615a227c1d831b55362a506
Shubham Goyal <shubham.goyal@hyperdart.com>
2019-09-11 19:25:26 +0530

    Added event error handling and validation

--- routes/eventDescRoutes/writeEventDesc.go
+++ routes/eventDescRoutes/writeEventDesc.go
@@ -1,12 +1,15 @@
 package eventDescRoutes
 
 import (
+	"context"
+	"encoding/json"
+	"errors"
+	"net/http"
+
 	"../../common/structs"
 	. "../../db"
 	"../../protocol"
-	"context"
-	"encoding/json"
-	"net/http"
+	"cloud.google.com/go/firestore"
 )
 
 func writeEvent(w http.ResponseWriter, r *http.Request) {
@@ -40,9 +43,28 @@ func addEvent(r *http.Request) error {
 		return err
 	}
 
+	errEVentInvalid := isEventValid(event)
+	if errEVentInvalid != nil {
+		return errEVentInvalid
+	}
+
 	errInAdding := addEventInFireStore(event)
 	if err != nil {
 		return errInAdding
+	}
+	return nil
+}
+
+func isEventValid(event structs.Event) error {
+	if event.Category == "" {
+		err := errors.New("Error in adding event: Category value can not be undefined")
+		return err
+	} else if event.Name == "" {
+		err := errors.New("Error in adding event: Event name can not be undefined")
+		return err
+	} else if event.DisplayName == "" {
+		err := errors.New("Error in adding event: Event display name can not be undefined")
+		return err
 	}
 	return nil
 }
@@ -60,8 +82,25 @@ func getEventObject(r *http.Request, event *structs.Event) error {
 func addEventInFireStore(event structs.Event) error {
 	fireStoreClient := GetFirestore()
 
-	_, err := fireStoreClient.Collection("events").Doc("eventDesc").Collection(event.Category).Doc(event.Name).Set(context.Background(), event)
-
+	err := addEventInEventDesc(event, fireStoreClient)
+	if err != nil {
+		return err
+	}
+	err = addEventInEventName(event, fireStoreClient)
 	return err
 }
 
+func addEventInEventDesc(event structs.Event, fireStoreClient *firestore.Client) error {
+	_, err := fireStoreClient.Collection("events").Doc("eventDesc").Collection(event.Category).Doc(event.Name).Set(context.Background(), event)
+	return err
+}
+
+func addEventInEventName(event structs.Event, fireStoreClient *firestore.Client) error {
+	newEvent := structs.Event{
+		Name:        event.Name,
+		DisplayName: event.DisplayName,
+		Category:    event.Category,
+	}
+	_, err := fireStoreClient.Collection("events").Doc("eventsName").Collection(event.Category).Doc(event.Name).Set(context.Background(), newEvent)
+	return err
+}
